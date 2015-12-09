0c67299701605e7f79696b74f9530a5f64d3597f
CMGS <ilskdw@gmail.com>
2015-12-09 16:43:27 +0800

    use base path in config

--- armin/config.py
+++ armin/config.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 #coding:utf-8
 
+import os
+
+BASE_PATH = os.path.dirname(os.path.dirname(__file__))
+
 INTERFACE_CONFIG = '/etc/sysconfig/network-scripts/ifcfg-%s'
 GATEWAY_CONFIG = 'GATEWAY=%s'
 
@@ -9,11 +13,11 @@ HOSTS_CONF = '/etc/hosts'
 
 EXECUTE_OK = 0
 
-LOCAL_REPO_DIR = '/Users/CMGS/Documents/Workplace/work/armin2/repos/'
+LOCAL_REPO_DIR = os.path.join(BASE_PATH, 'repos')
 REMOTE_REPO_DIR = '/etc/yum.repos.d/'
 
 USERADD_BIN = 'adduser'
-PKEY_DIR = '/Users/CMGS/Documents/Workplace/work/armin2/keys/'
+PKEY_DIR = os.path.join(BASE_PATH, 'keys')
 REMOTE_HOME_DIR = '/home'
 REMOTE_NOLOGIN_HOME_DIR = '/var/lib'
 
@@ -27,13 +31,13 @@ SSHD_CONFIG = '/etc/ssh/sshd_config'
 
 PASSWORD_LENGTH = 8
 
-ARMIN_PUB_KEY = '/Users/CMGS/Documents/Workplace/work/armin2/root/root.pub'
+ARMIN_PUB_KEY = os.path.join(BASE_PATH, 'root', 'root.pub')
 
-LOCAL_ULIMIT_CONF = '/Users/CMGS/Documents/Workplace/work/armin2/optimize/limits.conf'
+LOCAL_ULIMIT_CONF = os.path.join(BASE_PATH, 'optimize', 'limits.conf')
+LOCAL_SYSCTL_FILE = os.path.join(BASE_PATH, 'optimize', 'sysctl.py')
 REMOTE_ULIMIT_CONF = '/etc/security/limits.conf'
-LOCAL_SYSCTL_FILE = '/Users/CMGS/Documents/Workplace/work/armin2/optimize/sysctl.py'
 
-LOCAL_SERVICES_DIR = '/Users/CMGS/Documents/Workplace/work/armin2/services'
+LOCAL_SERVICES_DIR = os.path.join(BASE_PATH, 'services')
 
 FALCON_AGENT_CONF = 'cfg.json'
 FALCON_AGENT_REMOTE_PATH = '/etc/falcon-agent/cfg.json'
